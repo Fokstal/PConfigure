658ab9fa9b8d2613b2ee4387c122d5e1a8a07d33
Fokstal <klabu2016@mail.ru>
2023-05-03 18:37:29 +0300

    feat: layout StartupWindow (Wed, 03.05.2023 18:37:30)

--- PConfigure/View/StartupWindow.xaml.cs
+++ PConfigure/View/StartupWindow.xaml.cs
@@ -18,9 +18,9 @@ namespace PConfigure
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class StartupWindow : Window
 	{
-		public MainWindow()
+		public StartupWindow()
 		{
 			InitializeComponent();
 		}
